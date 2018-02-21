b2b97b9f5f599a1ebfc6b0b77f586092842c2478
Prasad <Rprgotu@gmail.com>
2018-02-21 23:55:10 +0530

    Final

---
+++ EmployeePortal-Assignment/Models/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeePortal_Assignment.Models
+{
+    public class Employee
+    {
+        // Scalar Properties
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmpId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [Remote("doesUserNameExist", "Account", HttpMethod = "POST", ErrorMessage = "User name already exists. Please enter a different user name.")]
+        public string UserName { get; set; }
+        [Required]
+        public string LoginUserName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DOJ { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public int DeptRefId { get; set; }
+
+        //// Navigation Property
+        [ForeignKey("DeptRefId")]
+        public Departments Department { get; set; }
+    }
+}
---
+++ EmployeePortal-Assignment/Models/SampleData.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EmployeePortal_Assignment.DB_Context;
+
+namespace EmployeePortal_Assignment.Models
+{
+    public class SampleData : CreateDatabaseIfNotExists<DBContext>
+    {
+        protected override void Seed(DBContext context)
+        {
+
+            try
+            {
+                List<Departments> d = new List<Departments>()
+                {
+                     new Departments {
+                         Name = "HR", Location = "USA",
+                     Employees = new List<Employee>() {
+                     new Employee()
+                     {
+                         FirstName = "Mark",
+                        LastName = "Hastings",
+                        UserName = "mark",
+                        LoginUserName = "mark",
+                        Gender = "Male",
+                        Password = "mark123",
+                        ConfirmPassword = "mark123",
+                        DOJ = Convert.ToDateTime("02/02/2017"),
+                        CreatedDate = DateTime.Now,
+                        EmailID = "mark@hotmail.com",
+                     } } },
+                     new Departments {
+                         Name = "Finance", Location = "Australia",
+                     Employees = new List<Employee>() {
+                     new Employee()
+                     {
+                         FirstName = "Sonam",
+                        LastName = "Gupta",
+                        UserName = "Sonam",
+                        LoginUserName = "Sonam",
+                        Gender = "Female",
+                        Password = "Sonam",
+                        ConfirmPassword = "Sonam",
+                        DOJ = Convert.ToDateTime("01/01/2017"),
+                        CreatedDate = DateTime.Now,
+                        EmailID = "Sonam@yahoo.com",
+                     } } },
+                     new Departments {
+                         Name = "Marketing", Location = "India",
+                     Employees = new List<Employee>() {
+                     new Employee()
+                     {
+                         FirstName = "Fahim",
+                        LastName = "Bhatt",
+                        UserName = "Fahim",
+                        LoginUserName = "Fahim",
+                        Gender = "Male",
+                        Password = "fahim",
+                        ConfirmPassword = "fahim",
+                        DOJ = Convert.ToDateTime("01/01/2017"),
+                        CreatedDate = DateTime.Now,
+                        EmailID = "Fahim@yahoo.com",
+                     } } },
+                     new Departments {
+                         Name = "Research & Development", Location = "Japan",
+                     Employees = new List<Employee>() {
+                     new Employee()
+                     {
+                        FirstName = "Kiran",
+                        LastName = "Sahu",
+                        UserName = "Kiran",
+                        LoginUserName = "Kiran",
+                        Gender = "Male",
+                        Password = "Kiran",
+                        ConfirmPassword = "Kiran",
+                        DOJ = Convert.ToDateTime("01/01/2017"),
+                        CreatedDate = DateTime.Now,
+                        EmailID = "Kiran@gmail.com",
+                     } } }
+                };
+
+                foreach (Departments std in d)
+                {
+                    context.Departments.Add(std);
+
+                }
+
+
+
+                context.SaveChanges();
+                base.Seed(context);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+        }
+    }
+}
